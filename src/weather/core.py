a56235dacec0aa92cb5068109ca335ee58f4352b
Andrew Nolan <andrewnolan871@gmail.com>
2025-02-19 19:54:45 +0000

    api is safe af

--- src/weather/core.py
+++ src/weather/core.py
@@ -44,13 +44,16 @@ https://archive-api.open-meteo.com/v1/archive
 '''
 
 from abc import ABC, abstractmethod
-from ..utils import enforce_types
-from . import weather_variable_enums
+# from ..utils import enforce_types
+# from . import weather_variable_enums
+from .weather_variable_enums import ForecastHourly, ForecastDaily, HistoricalHourly, HistoricalDaily, ForecastCurrent, ForecastMinutely15
 from . import enums
 import datetime as dt
 
+from beartype import beartype
+
 class OpenMeteoAPI(ABC):
-    @enforce_types
+    @beartype
     def __init__(self,base_url:str):
         self.base_url:str = base_url
 
@@ -58,8 +61,8 @@ class OpenMeteoAPI(ABC):
         self._latitude:float|None = None #required
         self._longitude:float|None = None #required
         self._elevation:float|None = None
-        self._hourly:str|None = None # must come from weather_variable_enums
-        self._daily:str|None = None  # must come from weather_variable_enums
+        self._hourly:None|list[ForecastHourly]|list[HistoricalHourly] = None # must come from weather_variable_enums
+        self._daily:None|list[ForecastDaily]|list[HistoricalDaily] = None  # must come from weather_variable_enums
         self._temperature_unit:enums.TemperatureUnit|None = None
         self._wind_speed_unit:enums.WindSpeedUnit|None = None
         self._precipitation_unit:enums.PrecipitationUnit|None = None
@@ -70,6 +73,12 @@ class OpenMeteoAPI(ABC):
         self._cell_selection:enums.CellSelection|None = None
         self._apikey:str|None = None
 
+    def __str__(self):
+        return '\n'.join(attr + ': ' + str(getattr(self, attr)) for attr in dir(self) if not attr.startswith('_') and getattr(self, attr) is not None)
+
+    def __repr__(self):
+        return self.__str__()
+
     # not allowed to be none.
     @property
     def latitude(self)->float|None:
@@ -78,7 +87,7 @@ class OpenMeteoAPI(ABC):
         return self._latitude
     
     @latitude.setter
-    @enforce_types
+    @beartype
     def latitude(self,value:float):
         self._latitude = value
     
@@ -89,7 +98,7 @@ class OpenMeteoAPI(ABC):
         return self._longitude
     
     @longitude.setter
-    @enforce_types
+    @beartype
     def longitude(self,value:float):
         self._longitude = value
 
@@ -99,7 +108,7 @@ class OpenMeteoAPI(ABC):
         return self._elevation
     
     @elevation.setter
-    @enforce_types
+    @beartype
     def elevation(self,value:float|None):
         self._elevation = value
 
@@ -108,7 +117,7 @@ class OpenMeteoAPI(ABC):
         return self._temperature_unit.value if self._temperature_unit is not None else None
     
     @temperature_unit.setter
-    @enforce_types
+    @beartype
     def temperature_unit(self,value:enums.TemperatureUnit|None):
         self._temperature_unit = value
 
@@ -117,7 +126,7 @@ class OpenMeteoAPI(ABC):
         return self._wind_speed_unit.value if self._wind_speed_unit is not None else None
 
     @wind_speed_unit.setter
-    @enforce_types
+    @beartype
     def wind_speed_unit(self,value:enums.WindSpeedUnit|None):
         self._wind_speed_unit = value
 
@@ -126,7 +135,7 @@ class OpenMeteoAPI(ABC):
         return self._precipitation_unit.value if self._precipitation_unit is not None else None
 
     @precipitation_unit.setter
-    @enforce_types
+    @beartype
     def precipitation_unit(self,value:enums.PrecipitationUnit|None):
         self._precipitation_unit = value
 
@@ -135,7 +144,7 @@ class OpenMeteoAPI(ABC):
         return self._timeformat.value if self._timeformat is not None else None
     
     @timeformat.setter
-    @enforce_types
+    @beartype
     def timeformat(self,value:enums.TimeFormat|None):
         self._timeformat = value
 
@@ -144,7 +153,7 @@ class OpenMeteoAPI(ABC):
         return self._timezone.value if self._timezone is not None else None
 
     @timezone.setter
-    @enforce_types
+    @beartype
     def timezone(self,value:enums.TimeZone|None):
         self._timezone = value
     
@@ -153,7 +162,7 @@ class OpenMeteoAPI(ABC):
         return self._start_date.strftime('%Y-%m-%d') if self._start_date is not None else None
     
     @start_date.setter
-    @enforce_types
+    @beartype
     def start_date(self,value:dt.datetime|None):
         self._start_date = value
 
@@ -162,7 +171,7 @@ class OpenMeteoAPI(ABC):
         return self._end_date.strftime('%Y-%m-%d') if self._end_date is not None else None
     
     @end_date.setter
-    @enforce_types
+    @beartype
     def end_date(self,value:dt.datetime|None):
         self._end_date = value
 
@@ -171,7 +180,7 @@ class OpenMeteoAPI(ABC):
         return self._cell_selection.value if self._cell_selection is not None else None
     
     @cell_selection.setter
-    @enforce_types
+    @beartype
     def cell_selection(self,value:enums.CellSelection|None):
         self._cell_selection = value
 
@@ -180,37 +189,10 @@ class OpenMeteoAPI(ABC):
         return self._apikey
     
     @apikey.setter
-    @enforce_types
+    @beartype
     def apikey(self,value:str|None):
         self._apikey = value
     
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
 class ForecastAPI(OpenMeteoAPI):
     '''
     latitude                :on request:required     
@@ -227,24 +209,181 @@ class ForecastAPI(OpenMeteoAPI):
     precipitation_unit      :on init  
     timeformat              :on init  
     '''
-    @enforce_types
+    @beartype
     def __init__(self):
         super().__init__('https://api.open-meteo.com/v1/forecast')
-        self._start_hour = None
-        self._end_hour = None
-        self._start_minutely_15 = None
-        self._end_minutely_15 = None
-        self._models = None
-        self._current = None
-        self._minutely_15 = None
-        self._past_days = None
-        self._forecast_days = None
-        self._forecast_hours = None
-        self._forecast_minutely_15 = None
-        self._past_hours = None
-        self._past_minutely_15 = None                
+        self._start_hour:dt.datetime|None = None
+        self._end_hour:dt.datetime|None = None
+        self._start_minutely_15:dt.datetime|None = None
+        self._end_minutely_15:dt.datetime|None = None
+        self._models:list[enums.Models]|None = None
+        self._current:list[ForecastCurrent]|None = None
+        self._minutely_15:list[ForecastMinutely15]|None = None
+        self._past_days:int|None = None
+        self._forecast_days:int|None = None
+        self._forecast_hours:int|None = None
+        self._forecast_minutely_15:int|None = None
+        self._past_hours:int|None = None
+        self._past_minutely_15:int|None = None
+
+
+    @property
+    def hourly(self)->list[str]|None:
+        return [_.value for _ in self._hourly] if self._hourly is not None else None
+    
+    @hourly.setter
+    @beartype
+    def hourly(self,value:list[ForecastHourly]|None):
+        self._hourly = value
+
+    @property
+    def daily(self)->list[str]|None:
+        return [_.value for _ in self._daily] if self._daily is not None else None
+    
+    @daily.setter
+    @beartype
+    def daily(self,value:list[ForecastDaily]|None):
+        self._daily = value
+
+    @property
+    def current(self)->list[str]|None:
+        return [_.value for _ in self._current] if self._current is not None else None
+    
+    @current.setter
+    @beartype
+    def current(self,value:list[ForecastCurrent]|None):
+        self._current = value
+
+    @property
+    def minutely_15(self)->list[str]|None:
+        return [_.value for _ in self._minutely_15] if self._minutely_15 is not None else None
+
+    @minutely_15.setter
+    @beartype
+    def minutely_15(self,value:list[ForecastMinutely15]|None):
+        self._minutely_15 = value
+
+    @property
+    def models(self)->list[str]|None:
+        return [_.value for _ in self._models] if self._models is not None else None
+    
+    @models.setter
+    @beartype
+    def models(self,value:list[enums.Models]|None):
+        self._models = value
+
+    @property
+    def start_hour(self)->str|None:
+        return self._start_hour.strftime('%Y-%m-%dT%H:%M') if self._start_hour is not None else None
+    
+    @start_hour.setter
+    @beartype
+    def start_hour(self,value:dt.datetime|None):
+        self._start_hour = value
+    
+    @property
+    def end_hour(self)->str|None:
+        return self._end_hour.strftime('%Y-%m-%dT%H:%M') if self._end_hour is not None else None
+    
+    @end_hour.setter
+    @beartype
+    def end_hour(self,value:dt.datetime|None):
+        self._end_hour = value
+
+    @property
+    def start_minutely_15(self)->str|None:
+        return self._start_minutely_15.strftime('%Y-%m-%dT%H:%M') if self._start_minutely_15 is not None else None
+    
+    @start_minutely_15.setter
+    @beartype
+    def start_minutely_15(self,value:dt.datetime|None):
+        self._start_minutely_15 = value
+
+    @property
+    def end_minutely_15(self)->str|None:
+        return self._end_minutely_15.strftime('%Y-%m-%dT%H:%M') if self._end_minutely_15 is not None else None
+    
+    @end_minutely_15.setter
+    @beartype
+    def end_minutely_15(self,value:dt.datetime|None):
+        self._end_minutely_15 = value
+
+    @property
+    def past_days(self)->int|None:
+        return self._past_days
+    
+    @past_days.setter
+    @beartype
+    def past_days(self,value:int|None):
+        self._past_days = value
+
+    @property
+    def forecast_days(self)->int|None:
+        return self._forecast_days
+    
+    @forecast_days.setter
+    @beartype
+    def forecast_days(self,value:int|None):
+        self._forecast_days = value
+
+    @property
+    def forecast_hours(self)->int|None:
+        return self._forecast_hours
+    
+    @forecast_hours.setter
+    @beartype
+    def forecast_hours(self,value:int|None):
+        self._forecast_hours = value
+
+    @property
+    def forecast_minutely_15(self)->int|None:
+        return self._forecast_minutely_15
+    
+    @forecast_minutely_15.setter
+    @beartype
+    def forecast_minutely_15(self,value:int|None):
+        self._forecast_minutely_15 = value
+
+    @property
+    def past_hours(self)->int|None:
+        return self._past_hours
+    
+    @past_hours.setter
+    @beartype
+    def past_hours(self,value:int|None):
+        self._past_hours = value
+
+    @property
+    def past_minutely_15(self)->int|None:
+        return self._past_minutely_15
+    
+    @past_minutely_15.setter
+    @beartype
+    def past_minutely_15(self,value:int|None):
+        self._past_minutely_15 = value
+
+                
 
 class HistoricalAPI(OpenMeteoAPI):
-    @enforce_types
+    @beartype
     def __init__(self):
         super().__init__('https://archive-api.open-meteo.com/v1/archive')
+    
+    @property
+    def hourly(self)->list[str]|None:
+        return [_.value for _ in self._hourly] if self._hourly is not None else None
+    
+    @hourly.setter
+    @beartype
+    def hourly(self,value:list[HistoricalHourly]|None):
+        self._hourly = value
+
+    @property
+    def daily(self)->list[str]|None:
+        return [_.value for _ in self._daily] if self._daily is not None else None
+
+    @daily.setter
+    @beartype
+    def daily(self,value:list[HistoricalDaily]|None):
+        self._daily = value
+
