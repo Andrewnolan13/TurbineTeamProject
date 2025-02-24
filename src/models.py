982e641a69011629612bd98b75c4a444df5139ba
andrew nolan <andrewnolan871@gmail.com>
2025-02-24 19:00:57 +0000

    messing w MLP

--- src/models.py
+++ src/models.py
@@ -5,15 +5,31 @@ this file will eventually have LTSM too
 import torch.nn as nn
 
 
+# class FeedForwardNN(nn.Module):
+#     def __init__(self, input_size, hidden_size=32):
+#         super(FeedForwardNN, self).__init__()
+#         self.fc1 = nn.Linear(input_size, hidden_size)
+#         self.relu = nn.ReLU()
+#         self.fc2 = nn.Linear(hidden_size, 1)  # Output layer (1 neuron for power output)
+
+#     def forward(self, x):
+#         x = self.fc1(x)
+#         x = self.relu(x)
+#         x = self.fc2(x)
+#         return x
+    
 class FeedForwardNN(nn.Module):
     def __init__(self, input_size, hidden_size=32):
-        super(FeedForwardNN, self).__init__()
+        super().__init__()
         self.fc1 = nn.Linear(input_size, hidden_size)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_size, 1)  # Output layer (1 neuron for power output)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, 1)
+        self.activation = nn.Tanh()
 
     def forward(self, x):
         x = self.fc1(x)
-        x = self.relu(x)
+        x = self.activation(x)
         x = self.fc2(x)
+        x = self.activation(x)
+        x = self.fc3(x)
         return x
